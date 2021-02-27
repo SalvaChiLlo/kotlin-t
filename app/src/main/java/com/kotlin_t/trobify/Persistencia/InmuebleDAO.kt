12c263f00d414b0aa5d92b0df0367c4334dc4623
SalvaChLl <salvachll@gmail.com>
2021-02-27 13:05:38 +0100

    Configuración básica del proyecto

--- app/src/main/java/com/kotlin_t/trobify/Persistencia/InmuebleDAO.kt
+++ app/src/main/java/com/kotlin_t/trobify/Persistencia/InmuebleDAO.kt
@@ -1,4 +1,31 @@
 package com.kotlin_t.trobify.Persistencia
 
+import androidx.room.*
+import com.kotlin_t.trobify.Logica.Inmueble
+
+@Dao
 interface InmuebleDAO {
+    @Query("SELECT * FROM inmueble")
+    fun getAll(): List<Inmueble>
+
+    @Query("SELECT * FROM inmueble WHERE id IN (:ids)")
+    fun loadAllByIds(ids: Set<Int>): List<Inmueble>
+
+    @Query("SELECT * FROM inmueble WHERE id like :id")
+    fun findById(id: String): Inmueble
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertAll(vararg inmuebles: Inmueble)
+
+    @Delete
+    fun delete(inmueble: Inmueble)
+
+    @Query("DELETE FROM inmueble")
+    fun deleteAll()
+
+    @Update
+    fun update(inmueble: Inmueble)
+
+    @Query("DELETE FROM inmueble WHERE id = :id")
+    fun deleteById(id: String)
 }
