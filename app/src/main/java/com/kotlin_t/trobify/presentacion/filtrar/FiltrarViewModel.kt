618bea6e055faeba169e14f38527bfbab4972de0
SalvaChLl <salvachll@gmail.com>
2021-03-08 11:40:02 +0100

    Layout de filtrado creado

---
+++ app/src/main/java/com/kotlin_t/trobify/presentacion/filtrar/FiltrarViewModel.kt
@@ -0,0 +1,9 @@
+package com.kotlin_t.trobify.presentacion.filtrar
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.kotlin_t.trobify.database.AppDatabase
+
+class FiltrarViewModel(val database: AppDatabase, application: Application) :
+    AndroidViewModel(application) {
+}
