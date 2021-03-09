36f3c9ddfb657e9123bdd577921f365251583af5
angelrm00 <angelromerom00@gmail.com>
2021-03-09 20:56:21 +0100

    Mostrar ficha incompleto

--- app/src/main/java/com/kotlin_t/trobify/presentacion/ficha/FichaViewModel.kt
+++ app/src/main/java/com/kotlin_t/trobify/presentacion/ficha/FichaViewModel.kt
@@ -4,10 +4,5 @@ import androidx.lifecycle.ViewModel
 import com.kotlin_t.trobify.logica.Inmueble
 
 class FichaViewModel : ViewModel() {
-    lateinit var inmueble : Inmueble
 
-
-    fun setInmueble(setter: Inmueble) {
-        this.inmueble = setter
-    }
 }
