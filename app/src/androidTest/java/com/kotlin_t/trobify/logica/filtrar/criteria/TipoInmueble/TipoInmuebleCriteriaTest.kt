88e492ece73260eaab070dbf2cb0d56e84ac8ca6
SalvaChLl <salvachll@gmail.com>
2021-05-28 18:11:36 +0200

    Pruebas unitarias

---
+++ app/src/androidTest/java/com/kotlin_t/trobify/logica/filtrar/criteria/TipoInmueble/TipoInmuebleCriteriaTest.kt
@@ -0,0 +1,12 @@
+package com.kotlin_t.trobify.logica.filtrar.criteria.TipoInmueble
+
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class TipoInmuebleCriteriaTest {
+
+    @Before
+    fun setUp() {
+    }
+}
