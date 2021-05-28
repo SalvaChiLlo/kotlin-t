451253ab5f9e8d3985aaf7e7a4b4bbfda071bd4e
chris <christiangarcia11598@gmail.com>
2021-05-28 18:50:43 +0200

    Fuente del Texto BIENVENIDO reducido, y ahora en Username de Login no se pueden escribir espacios

--- app/src/main/java/com/kotlin_t/trobify/MainActivity.kt
+++ app/src/main/java/com/kotlin_t/trobify/MainActivity.kt
@@ -63,12 +63,6 @@ class MainActivity : AppCompatActivity(){
         populate()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
 
